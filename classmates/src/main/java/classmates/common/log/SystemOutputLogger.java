2ec74e93d7c9c03ff56c90637667654eaa03fe78
akrasnov <akrasnov@AKRASNOV-PC.sw.swsoft.com>
2012-06-13 05:12:46 +0700

    add utf8 logger

--- classmates/src/main/java/classmates/common/log/SystemOutputLogger.java
+++ classmates/src/main/java/classmates/common/log/SystemOutputLogger.java
@@ -1,8 +1,10 @@
 package classmates.common.log;
 
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 
 /**
  * Trivial logger to system output
@@ -15,6 +17,10 @@ public class SystemOutputLogger
   {
     logger = Logger.getRootLogger();
     BasicConfigurator.configure();
+    ConsoleAppender appender = new ConsoleAppender();
+    appender.setLayout(new PatternLayout("%-5p [%t]: %m%n"));
+    appender.setEncoding( "UTF-8" );
+    BasicConfigurator.configure( appender );
     logger.setLevel( Level.INFO );
   }
 
